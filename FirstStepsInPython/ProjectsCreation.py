655b749ed5ab72dd973a25e156289c3b898f1707
Yvonne Ivanova <160223911+Yvonneiv@users.noreply.github.com>
2024-03-10 12:35:54 +0200

    task8

---
+++ FirstStepsInPython/ProjectsCreation.py
@@ -0,0 +1,7 @@
+name = input()
+projects_count = int(input())
+
+hours_needed = projects_count * 3
+
+print(f'The architect {name} will need {hours_needed} hours to complete {projects_count} project/s.')
+
